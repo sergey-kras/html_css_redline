e6203fb745ebb33cccab4b899be8d5bf873ecec5
sergey-kras <vip.41243@gmail.com>
2018-05-11 03:16:54 +0600

    Сделал анимацию блока с товарами

--- app/js/common.js
+++ app/js/common.js
@@ -14,7 +14,6 @@ $('body').on('mousemove',function (e) {
     var height = block.height();
     var xPosition = e.pageX;
     var yPosition = e.pageY;
-    checkPosition();
     block.css({
         'background-position-x': width  / 2 * 0.1 -  width * 0.07  - xPosition * 0.07,
         'background-position-y': height / 2 * 0.1 - height * 0.07 - yPosition * 0.03
@@ -27,3 +26,15 @@ function checkPosition() {
     });
 }
 checkPosition();
+setInterval(checkPosition(),1);
+
+var $win = $(window);
+var $block = $('.products__product');
+$win.scroll(function() {
+    for (var $i = 0; $i < $block.length ; $i++) {
+        if ($win.scrollTop() + $win.height() >= $($block[$i]).offset().top - 1300) {
+            console.log($i);
+            $($block[$i]).animate({'margin-top': 30 + 'px'}, 1200);
+        }
+    }
+});
