bcd8c1c9097d8e1982864fe37710c7c021422982
sergey-kras <vip.41243@gmail.com>
2018-05-14 03:12:26 +0700

    Страница портфолио

--- app/js/common.js
+++ app/js/common.js
@@ -33,9 +33,9 @@ $win.scroll(function() {
     }
 });
 var blockMenu = $('.menu');
-var products = $('.products');
+var content = $('.content');
 $win.scroll(function() {
-    if ($win.scrollTop()  >= products.offset().top) {
+    if ($win.scrollTop()  >= content.offset().top) {
         blockMenu.css({position : 'fixed'})
     }
     else {
@@ -46,7 +46,7 @@ $win.scroll(function() {
 
 var burger = $('.menu__burger');
 var fade = $('.fade');
-var left = $('.products .container').offset().left - 15;
+var left = $('.content .container').offset().left - 15;
 var menu = $('.menu__nav');
 var menuFooter = $('.menu__footer');
 burger.css({'margin-left':left});
@@ -91,3 +91,42 @@ fade.on('click',function(){
     console.log(1);
     hideMenu();
 });
+
+
+
+// external js: isotope.pkgd.js
+
+// init Isotope
+var $grid = $('.container__works').isotope({
+    itemSelector: '.works__item',
+    layoutMode: 'fitRows'
+});
+// filter functions
+var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+        var number = $(this).find('.number').text();
+        return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+        var name = $(this).find('.name').text();
+        return name.match( /ium$/ );
+    }
+};
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $( this ).addClass('is-checked');
+    });
+});
+
