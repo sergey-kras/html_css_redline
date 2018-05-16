bfb176fa3a8004cb883576a0e48ef700d76432fa
sergey-kras <vip.41243@gmail.com>
2018-05-16 10:43:57 +0700

    Готова страница с новостями

--- dist/form.php
+++
@@ -1,9 +0,0 @@
-<?php
-$name = $_GET['name'];
-$product = $_GET['ask'];
-$tel = $_GET['tel'];
-$message = "
-Имя: {$name} 
-Телефон: {$tel}
-Товар: {$product}";
-mail('elena-gold2005@mail.ru', 'Новый заказ', $message);
